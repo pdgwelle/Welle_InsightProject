a49adea1d834fed9071c95d2c2cde77c3f9418cc
pdgwelle <pdgwelle@gmail.com>
2018-01-22 14:11:48 -0500

    Remove stop words from words

--- database_scripts/add_words_to_db.py
+++ database_scripts/add_words_to_db.py
@@ -1,6 +1,14 @@
+import string
 from datetime import datetime
 
+from nltk.corpus import stopwords
+
 import model
+
+def delete_all_chars(word, chars):
+    for char in chars:
+        word = word.replace(char, '')
+    return word
 
 if __name__ == '__main__':
 
@@ -11,11 +19,14 @@ if __name__ == '__main__':
 
     model.Word.objects.delete()
 
+    stop_words = set(stopwords.words('english'))
+    stop_words = map(lambda x: delete_all_chars(x, string.punctuation), stop_words)
+
     with open('words.txt', 'r') as f:
         words = []
         for line in f:
             line = line.strip().lower()
-            if(len(line) >= min_word_length):
+            if((len(line) >= min_word_length) and (line not in stop_words)):
                 word_object = model.Word(word=line).save()
 
     tend = datetime.now()
