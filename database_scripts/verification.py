331aa36b494ae41bd57766c9e389a165cb5d9b0a
pdgwelle <pdgwelle@gmail.com>
2018-01-24 14:25:18 -0500

    Add verification for news articles

--- database_scripts/verification.py
+++ database_scripts/verification.py
@@ -1,3 +1,6 @@
+import sys
+
+import numpy as np
 import pandas as pd
 
 import sklearn as skl
@@ -6,19 +9,32 @@ from sklearn.metrics import accuracy_score
 
 import model
 
-def create_Xy(parent_docs):
+def create_Xy(parent_docs, doctype):
     X = []
     y = []
     for doc in parent_docs:
         X.extend([list(passage.document_embedding) for passage in doc.passages])
-        y.extend([doc.unique_field]*len(doc.passages))
+        if(doctype == 'book'): 
+            y.extend([doc.unique_field]*len(doc.passages))
+        else:
+            y.extend([doc.author]*len(doc.passages))
     X = pd.DataFrame(X)
     y = pd.Series(pd.factorize(y)[0])
     return X,y
 
 if __name__ == '__main__':
-    parent_docs = model.Parent_Document.objects()
-    X,y = create_Xy(parent_docs)
+    if(len(sys.argv) == 1):
+        print("Please add doctype of either 'article' or 'book'")
+        sys.exit()
+
+    doctype = sys.argv[1]
+    if(doctype not in ['article', 'book']):
+        print("Doctype not 'article' or 'book'")
+        sys.exit()
+
+    parent_docs = model.Parent_Document.objects(doctype=doctype)
+
+    X,y = create_Xy(parent_docs, doctype)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4512)
 
@@ -26,13 +42,15 @@ if __name__ == '__main__':
     nb = GaussianNB()
     nb.fit(X_train, y_train)
     y_pred = nb.predict(X_test)
-    accuracy_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Naive Bayes accuracy " + str(accuracy))
 
     from sklearn.ensemble import RandomForestClassifier
     rf = RandomForestClassifier(n_estimators=100, n_jobs=3)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
-    accuracy_score(y_test, y_pred)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Random Forest accuracy " + str(accuracy))
 
     # from sklearn.model_selection import GridSearchCV
     # rf = RandomForestClassifier(n_estimators=100, n_jobs=3)
